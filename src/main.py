3eca6581934a3b5eeafbca25f63a5c780f4732ff
Nguyen Minh Duc <nguyenminhduc1982005@gmail.com>
2026-03-06 12:11:33 +0700

    Feat: add video

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import cv2
 from detection.detect_player import PlayerDetector
 from tracking.sort_tracker import SortTracker
 
-VIDEO_PATH = "data/raw_video/tennis_input.mp4"
+VIDEO_PATH = "data/raw_video/tennis_input6.mp4"
 MODEL_PATH = "models/detection/yolo_player.pt"
 
 def main():
@@ -28,11 +28,11 @@ def main():
         detections = detector.detect(frame)
         tracked_players = tracker.update(detections)
 
-        # DEBUG visualization (tạm thời – tracking mnguoi sẽ xử lý sau)
+      
         for player in tracked_players:
             x1, y1, x2, y2, track_id = player
             
-            # Tạo màu khác nhau cho từng ID
+       
             color = (0, 255, 0) if track_id == 1 else (255, 0, 0)
             
             cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), color, 2)
